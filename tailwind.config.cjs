a28156f7c19484ecb10fc5a0b9ae6b8fd9085792
Alberte <albe304d@stud.kea.dk>
2022-08-25 15:49:26 +0200

    ny

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      overskrifter: "#A77979",
+      baggrund: "#704F4F",
+    },
+    extend: {
+      fontFamily: {
+        khand: "'Khand', system-ui, sans-serif",
+        redhat: "'Red Hat Text', sans-serif",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
